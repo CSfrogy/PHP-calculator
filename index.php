7d87303aecd81ea0f678b77c6be65c5b751b76ed
CSfrogy <lonelyguse@gmail.com>
2025-08-01 16:25:04 +0800

    Creating index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SimpleCount</title>
+</head>
+<body>
+    <div class="main-container">
+        <div class="input-container">
+            <h1>Please input number</h1>
+            <h2>Select operation</h2>
+        </div>
+    </div>
+</body>
+</html>
