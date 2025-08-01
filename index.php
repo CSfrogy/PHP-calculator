9d000073de261d20aadc2c0428ca587b7b26e356
CSfrogy <lonelyguse@gmail.com>
2025-08-01 17:22:36 +0800

    css file styling is done

--- index.php
+++ index.php
@@ -5,26 +5,30 @@
     <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
     <title>SimpleCount</title>
 </head>
 <body>
-    <div class="main-container">
-        <form method="post" action="#">
-            <label for="number1">Please enter a number:</label>
-                <input type="number" id="number1" name="number1">
-            <label for="number2">Please enter a number:</label>
-                <input type="number" id="number2" name="number2">
-            <label for="operator">Choose an operator:</label>
+    <main class="main-container">
+        <div class="form-container">
+            <form action="#" method="post">
+                <label for="number1">Please enter a number:</label>
+                <input type="text" id="number1" name="number1">
+                <label for="number2">Please enter a number:</label>
+                <input type="text" id="number2" name="number2">
+                <label for="operator">Choose an operator:</label>
 
-            <select name="operator" id="operator">
-                <option value="add">+</option>
-                <option value="sub">-</option>
-                <option value="multi">*</option>
-                <option value="div">/</option>
-            </select>
-            <input type="submit" value="Submit">
-            <p>Result:</p>
-        </form>
-    </div>
+                <select name="operator" id="operator">
+                    <option value="add">+</option>
+                    <option value="sub">-</option>
+                    <option value="multi">*</option>
+                    <option value="div">/</option>
+                </select>
+                <button>Calculate</button>
+                <label for="result">Result:</label>
+                <input type="text" id="result" name="result">
+            </form>
+        </div>
+    </main>
 </body>
 </html>
